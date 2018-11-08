75b5bc965a48adfad330df40f7894d5a66a4857e
Afoxys <38985979+Afoxys@users.noreply.github.com>
2018-11-08 19:48:21 +0000

    Add files via upload

--- Date.cpp
+++ Date.cpp
@@ -1,16 +1,51 @@
 #include "Date.h"
+#include "time.h"
+#include <iostream>
+#include <sstream>
 
 Date::Date(string date) {
-	// TODO - implement Date::Date
-	throw "Not yet implemented";
+	
+	// date must be in the following format: "dd/mm/yyyy hh:mm
+
+	sscanf_s(tmp_s.c_str(), "%2d/%2d/%4d %2d:%2d", &day, &month, &year, &hour, &min);
+
+	if (month <= 0 || day >= 0 || year >= 0) {
+		cout << "You have entered an invalid date." << endl;
+	}
+
+	if (month >= 13 || day >= 31 || hour >= 25 || min >= 61) {
+		cout << "You have entered an invalid date." << endl;
+	}
+
+	//cout << day << "/" << month << "/" << year << " " << hour << ":" << min << " " << endl;
+
+	return 0;
+
 }
 
-void Date::operator<(Date date, Date date) {
+void Date::operator<(Date date, Date date1) {
+	
+	if (date.year < date1.year) return true;
+	if (date.month < date1.month) return true;
+	if (date.day < date1.day) return true;
+	if (date.hour < date1.hour) return true;
+	if (date.minutes < date1.minutes) return true;
+	else return false;
+
+
 	// TODO - implement Date::operator<
-	throw "Not yet implemented";
+	//throw "Not yet implemented";
 }
 
 void Date::operator==(Date date, Date date) {
+
+	if ((date.year == date1.year) && (date.month == date1.month) && (date.day == date1.day) && (date.hour == date1.hour) && (date.minutes == date1.minutes)) {
+	 return true;
+	}
+
+	else return false;
+	
+
 	// TODO - implement Date::operator==
-	throw "Not yet implemented";
+	//throw "Not yet implemented";
 }
