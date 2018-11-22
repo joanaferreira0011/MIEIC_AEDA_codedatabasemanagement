84b32409f367f69b4ef297fedbe7e473ca8267ab
Afoxys <38985979+Afoxys@users.noreply.github.com>
2018-11-22 23:15:07 +0000

    Update Date.cpp

--- Projeto/AEDAPROJ1/Date.cpp
+++ Projeto/AEDAPROJ1/Date.cpp
@@ -20,11 +20,11 @@ Date::Date(string date) {
 	sscanf_s(date.c_str(), "%2d/%2d/%4d %2d:%2d", &day, &month, &year, &hour, &minutes); //! The scan function reads the date string and stores the date values 
 
 	if (month <= 0 || day >= 0 || year >= 0) {
-		throw InvalidDate();						//! Throw an error due to an invalid date
+		throw invalid_argument("The date is invalid");						//! Throw an error due to an invalid date
 	}
 
 	if (month >= 13 || day >= 32 || hour >= 25 || minutes >= 61) {
-		throw InvalidDate();			//! Throw an error due to an invalid date
+		throw invalid_argument("The date is invalid");			//! Throw an error due to an invalid date
 	}
 
 	
