325bf83577fa75a8819d3e33f33d5327c7196801
Afoxys <38985979+Afoxys@users.noreply.github.com>
2018-11-23 21:26:43 +0000

    Exception Class

---
+++ Error.cpp
@@ -0,0 +1,22 @@
+#include "stdafx.h"
+#include "Error.h"
+#include <iostream>
+
+using namespace std;
+
+Error::Error(int error)
+{
+	switch (error){
+
+	case 1:
+		cout << "Invalid Date" << endl;
+		break;
+	default:
+		break;
+	}
+}
+
+
+Error::~Error()
+{
+}
